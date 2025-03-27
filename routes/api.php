185890c7e513117fef7d1bc73d3b72d450cb806b
Tran Hoang Xuan Nguyen <hoangnguyendepgiai@gmail.com>
2025-03-27 10:19:01 +0700

    done

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::Resource('/products', 'ProductController');
+Route::prefix('products')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::post('/', [ProductController::class, 'store']);
+    Route::get('/{id}', [ProductController::class, 'show']);
+    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+});
